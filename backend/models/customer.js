d711704b5a1a2a1f3dc2276e34bfc07de1628ca0
Sagar Ganeshkar <sagarganeshkar1111@gmail.com>
2025-11-08 22:17:32 +0530

    Optimize fetch calls and remove unused console logs

--- backend/models/customer.js
+++ backend/models/customer.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-
+//This is customer model
+const mongoose = require("mongoose");
 const someModel = new mongoose.Schema({
-    name:String,
-    email: String,
-    password: String,
-    role: String,
-    ratedMess: String
+  name: String,
+  email: String,
+  password: String,
+  role: String,
+  ratedMess: String,
 });
 
 //Taking the model name customer, which automatically creates a collection named as 'customers'
-const customer = mongoose.model("customer",someModel);
+const customer = mongoose.model("customer", someModel);
 module.exports = customer;
